af255c729ea0f89c34703cd075fa3e021bca9a0d
GregShen <gregshen0925@gmail.com>
2022-11-22 02:03:06 +0800

    click outside close hook

--- src/components/TodoModal.tsx
+++ src/components/TodoModal.tsx
@@ -1,5 +1,6 @@
 import { Task } from "@prisma/client";
-import React, { Dispatch, SetStateAction, useState } from "react";
+import React, { Dispatch, SetStateAction, useRef, useState } from "react";
+import useOnClickOutside from "../hooks/useOnClickOutside";
 import { trpc } from "../utils/trpc";
 
 type Props = {
@@ -8,6 +9,11 @@ type Props = {
 };
 
 const TodoModal = ({ setTodoModalOpen, setTodos }: Props) => {
+  const clickOutsideRef = useRef<HTMLDivElement>(null);
+  const clickOutsidehandler = () => {
+    setTodoModalOpen(false);
+  };
+  useOnClickOutside(clickOutsideRef, clickOutsidehandler);
   const [input, setInput] = useState<string>("");
   const { mutate: addTask } = trpc.task.addTask.useMutation({
     onSuccess(data: Task) {
@@ -26,13 +32,13 @@ const TodoModal = ({ setTodoModalOpen, setTodos }: Props) => {
     setTodoModalOpen(false);
   };
   return (
-    <div className="absolute inset-0 flex items-center justify-center bg-black/75">
-      <div className="space-y-4 rounded-lg bg-white p-3 ">
-        <h3 className="text-xl font-semibold">Add Task</h3>
+    <div className="absolute inset-0 flex items-center justify-center bg-black/75 bg-opacity-80 backdrop-blur-sm">
+      <div className="space-y-4 rounded-lg p-3 " ref={clickOutsideRef}>
+        <h3 className="text-xl font-semibold text-white">Add Task</h3>
         <input
           type="text"
           onChange={handleChange}
-          className="w-full rounded-md border-[1px] border-black bg-gray-300 shadow-sm focus:border-violet-300 focus:ring"
+          className="w-full rounded-md border-[1px] border-black bg-gray-300 shadow-sm focus:border-blue-300 focus:ring"
         />
         <div className="grid grid-cols-2 gap-4">
           <button
@@ -43,7 +49,7 @@ const TodoModal = ({ setTodoModalOpen, setTodos }: Props) => {
           </button>
           <button
             onClick={handleAddTask}
-            className="rounded-xl bg-violet-500 p-2 text-xs text-white transition hover:bg-violet-600"
+            className="rounded-xl bg-blue-500 p-2 text-xs text-white transition hover:bg-blue-600"
           >
             Add
           </button>
